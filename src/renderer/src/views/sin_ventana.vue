23021e88bf8284a1a2b9d09c2aa4e09f6d21fd03
YushMC <mujushaxb10@gmail.com>
2024-09-13 11:28:32 -0600

    Solucion de carpeta no encontrada

--- src/renderer/src/views/sin_ventana.vue
+++ src/renderer/src/views/sin_ventana.vue
@@ -5,6 +5,17 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
+onMounted(async () => {
+    try{
+       const folderExists = await window.api.checkFolderExists();
+        if (!folderExists) {
+          router.push({ name: 'DDLC' }); // Redirigir a otra vista si la carpeta no existe
+        } 
+    }catch(errr){
+
+    }
+})
 
 </script>
 
